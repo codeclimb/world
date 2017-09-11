464f6af8e4d8e9743bff6946ccbc56e15b72e460
codeclimb <mpinco@outlook.com>
2017-09-11 11:07:35 -0600

    Add files via upload

---
+++ while_code_mp.rb
@@ -0,0 +1,30 @@
+
+$dollar_amt = 0
+
+def saved
+	if $dollar_amt >= 25 then
+		$dollar_amt = 0
+ 		return false
+ 	else
+		$dollar_amt += 2
+ 		return true
+ 	end 		
+end
+
+def saving
+	puts('You need more money!')
+end
+
+def funded
+ 	puts("$" * $dollar_amt )
+end
+
+while saved do funded
+end
+puts("Funding Goal COMPLETE!")
+
+begin 
+	funded
+	saving
+end while saved
+puts("Funding Goal COMPLETE!")
