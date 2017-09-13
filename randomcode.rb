49b625251c60b3e3f0e4c3330566f987f0608328
codeclimb <mpinco@outlook.com>
2017-09-13 10:13:50 -0600

    Add files via upload

---
+++ randomcode.rb
@@ -0,0 +1,12 @@
+#print ('what is your name:    ')
+print ('what is your age:    ')
+age = gets
+
+if age.is_a?(Integer)
+  puts ("your age is #{age}")
+else
+  puts ("you need to use a number!")
+end
+#yourage = age.to_i
+
+#puts ("hi there #{yourage}")
